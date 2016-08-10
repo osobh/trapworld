81ba3bbbd9f47750ad485e901a549ecb66441c70
Omar Sobh <om.sobh@gmail.com>
2016-08-10 00:20:41 +0000

    adding in graph logic

--- routes/graph.js
+++ routes/graph.js
@@ -223,25 +223,23 @@ var info = [];
 console.log("getting it")
 console.log(dataUrl)
 
-//Here we request the data from the URL we want to hit
-request(dataUrl, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      info.push(JSON.parse(body));
-      // console.log(info);
-    }
-    //console.log(info);
-    
-//Here we loop through the returned JSON file an dprovide an interface to extracting values from the entire dataset
-
-    for(let i = 0 ; i < info.length ; i++){
-      var newData = (info[i]);
-      for(let key in newData) {
-        var obj = newData[key];
-      console.log(obj.address);
+  //Here we request the data from the URL we want to hit
+  request(dataUrl, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        info.push(JSON.parse(body));
       }
-    }
-    
-});
+
+  //Here we loop through the returned JSON file an dprovide an interface to extracting values from the entire dataset
+  
+      for(let i = 0 ; i < info.length ; i++){
+        var newData = (info[i]);
+        for(let key in newData) {
+          var obj = newData[key];
+        console.log(obj.address);
+        }
+      }
+      
+  });
 // return info;
 }
 
